f6dbab6984bb2b0512f87efafd55f09d205909c6
bl-test-account <plugin.developer@bytelight.com>
2013-02-14 07:38:05 -0800

    Saved at: 2013-02-14 10:38:08 -0500

--- create/page.js
+++ create/page.js
@@ -1,13 +1,36 @@
-$(function() {
-  BL.scanProduct(function(data){
+var clearData = function() {
+  $('.image').addClass('invisible');
+  $('.title').text('');
+  $('.price').text('');
+  $('.description').text('');
+  $('.message').text('');
+}
+
+var scanned = function(data) {
     if(data.found){
+      $('.image').removeClass('invisible');
       $('.image').attr('src', data.item.product.images[0].link);
       $('.title').text(data.item.product.title);
       $('.price').text('$' + data.item.product.inventories[0].price);
       $('.description').text(data.item.product.description);
+      $('.message').text('');
+      $('#submit').removeClass('disabled-button');
     }
     else{
-      $('#name > strong').text("Product not found at walmart.com");
+      $('.message').text('Product not found');
     }
+}
+
+$(function() {
+  BL.scanProduct(function(data){
+    scanned(data);
   })
+  
+  $('#rescan').click(function(){
+    $('#submit').addClass('disabled-button');
+    clearData();
+    BL.scanProduct(function(data){
+      scanned(data);
+    })
+  });
 })
