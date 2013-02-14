a7708947f380d88ec664df9c5717d08a5a336f22
bl-test-account <plugin.developer@bytelight.com>
2013-02-13 16:48:04 -0800

    Saved at: 2013-02-13 19:48:05 -0500

--- create/page.js
+++ create/page.js
@@ -1,9 +1,14 @@
 $(function() {
   BL.scanProduct(function(data){
-    $('#name > strong').text(data.name);
-    $('#image').attr('src', data.image);
-    $('#price').text('$' + data.price);
-    $('#buy').text('View at walmart.com');
-    $('#buy').attr('href', data.url);
+    if(data.name.length > 0){
+      $('#name > strong').text(data.name);
+      $('#image').attr('src', data.image);
+      $('#price').text('$' + data.price);
+      $('#buy').text('View at walmart.com');
+      $('#buy').attr('href', data.url);
+    }
+    else{
+      $('#name > strong').text("Product not found at walmart.com");
+    }
   })
 })
